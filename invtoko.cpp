b03e69bc6eb1dbb3e8aaea1854c4b3c2242399fa
Muhammad Ashshadiqil Amin <muhammadashshadiqil@gmail.com>
2025-06-22 14:18:40 +0800

    Struktur Awal

---
+++ invtoko.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct toko{
+    int id;
+    string nama;
+    string kategori;
+    string harga;
+    int stok;
+    toko* next;
+};
+
+int main(){
+  
+}
